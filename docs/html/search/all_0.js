0a96214cece62232eaf81c7597e8b99c185527b5
YozNacks <duarte.a.arribas@gmail.com>
2021-05-22 13:44:19 +0100

    added docs

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['best_5fsold_0',['BEST_SOLD',['../structBEST__SOLD.html',1,'']]]
+];
---
+++ docs/html/search/classes_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['best_5fsold_1',['BEST_SOLD',['../structBEST__SOLD.html',1,'']]]
+];
